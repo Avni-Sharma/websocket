4883bf7bf3e5d5c982a8cf1743c42f37a512ead7
Avni Sharma <16avnisharma@gmail.com>
2019-07-08 17:43:42 +0530

    Practice

---
+++ hello.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return false },
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, _ := upgrader.Upgrade(w, r, nil)
+	go writer(ws)
+}
+func writer(ws *websocket.Conn) {
+	defer ws.Close()
+
+	for {
+		ws.WriteMessage(websocket.TextMessage, []byte("hello"))
+		time.Sleep(2 * time.Second)
+	}
+}
+func main() {
+	http.HandleFunc("/ws", serveWs)
+	http.ListenAndServe(":8081", nil)
+
+}
